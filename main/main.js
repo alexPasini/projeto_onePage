4b95bb028da5af203f8c84305db7707d9943fd96
Alex_Pasini <alexantoiopasini20@gmail.com>
2024-10-09 18:58:01 -0300

    add: finalizando

--- main/main.js
+++ main/main.js
@@ -1,5 +1,7 @@
-function copiarTexto() {
-  var texto = document.getElementById("texto").innerText;
+function copiarTexto(button) {
+  var walletDiv = button.closest(".wallet1, .wallet2, .wallet3");
+
+  var texto = walletDiv.getAttribute("data-value");
 
   var campoTemporario = document.createElement("textarea");
   campoTemporario.value = texto;
@@ -9,4 +11,6 @@ function copiarTexto() {
   document.execCommand("copy");
 
   document.body.removeChild(campoTemporario);
+
+  alert("Wallet copiada para a área de tranferência: " + texto);
 }
